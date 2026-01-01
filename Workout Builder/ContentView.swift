7d1d29650adc58297b4f95197efacad4b7faaf9b
Idan Garten <idan.garten@gmail.com>
2026-01-01 00:08:29 -0400

    Initial Commit

---
+++ Workout Builder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout Builder
+//
+//  Created by Idan Garten on 1/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout Builder/Workout_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_BuilderApp.swift
+//  Workout Builder
+//
+//  Created by Idan Garten on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Workout_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
